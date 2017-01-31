46d2a847a3072efff1a4d24694b490f290fde7a0
tingleshao <cshao@cs.unc.edu>
2017-01-31 12:44:39 -0500

    save update

--- makeSaccadeConfig2.py
+++ makeSaccadeConfig2.py
@@ -6,10 +6,40 @@ def parse(opencv_out, filename):
     # takes the <opencv_out> file as a string, parse it into a JSON file,
     # and write it into file with <filename>
 
+    x = 0
+    yaw = []
+    pitch = []
+    roll = []
+    k1 = []
+    offsetx = []
+    offsety = []
+    f = []
+    s = []
+
+    #TODO: expand the string to fill in these lists 
+
     with open("reference.json") as json_File:
         json_data = json.load(json_file, object_pairs_hook = OrderedDict)
-        
 
+    poly = [[0]] * 10
+    alist = [1]
+    poly.insert(0, alist)
+
+    for key in json_data.keys():
+        for key2 inf json_data[key]:
+            if key2 == "global":
+                json_data[key][key2]['vig_poly_red'] = poly
+                json_data[key][key2]['vig_poly_green'] = poly
+                json_data[key][key2]['vig_poly_blue'] = poly
+                json_data[key][key2]['sensorwidth'] = im.size[0]
+                json_data[key][key2]['sensorheight'] = im.size[1]
+                json_data[key][key2]['pixel_size'] = .0014
+                json_data[key][key2]['focal_length'] = 35
+            if key2 == "microcameras":
+                for i in xrange(len(json_data[key][key2])):
+                    json_data[key][key2][i] = {'Slot':int(s[i]), 'Sensorcal':["1", "1", "1", "1"], 'gain':1, 'vigoffset_x':0, 'vigoffset_y':0, 'Yaw': yaw[i], 'Pitch':pitch[i], 'Roll':roll[i], 'K1':k1[i], 'OFFSET_X':offsetx[i], 'OFFSET_Y':offsety[i], 'F':float(f[i])}
+                for i in xrange(len(s)):
+                    json_data[key][key2].append({'Slot': int(s[i]), 'Sensorcal': ["1", "1", "1", "1"], 'gain': 1, 'vigoffset_x': 0, 'vigoffset_y': 0, 'Yaw': yaw[i], 'Pitch': pitch[i], 'Roll': roll[i], 'K1': k1[i], 'OFFSET_X': offsetx[i], 'OFFSET_Y': offsety[i], 'F': float(f[i])})
 
     data = json.dumps(json_data, indent=4)
     with open(filename, "w") as outfile:
