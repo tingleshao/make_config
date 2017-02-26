a529258ee89b227b3b82b8d29d1451db556748ea
tingleshao <cshao@cs.unc.edu>
2017-02-26 12:37:05 -0500

    add new makeSaccadeConfig script

---
+++ makeSaccadeConfig3.py
@@ -0,0 +1,6 @@
+# the purpose of this is to compute the pitch, yaw and roll values to determine the images locations
+# we still get the ... focal length and K information from opencv ... ? But we compute pitch, yaw and roll values by our selves
+#
+
+def parse(opencv_out, filename):
+    return None
